1427e039fe26f7650e2b37c0e62ad1ede7ab9037
Andy Filchev <andyfilchev@protonmail.com>
2020-05-14 00:52:24 +0300

    feat: Update Map.elm view(html) and css

--- src/Page/Map.elm
+++ src/Page/Map.elm
@@ -424,23 +424,287 @@ viewMode model =
                     viewRoute model
 
 
-getSelectedLandmark : Model -> Maybe LandmarkSummary
-getSelectedLandmark model =
-    case model.selectedLandmarkSummary of 
-        Just id ->
-            Dict.get id model.landmarkSummaryList
 
-        Nothing -> 
-            Nothing
+-- ROUTE
 
 
-viewInfoControls : Html Msg
-viewInfoControls =
-    div [ id "info-controls-container" ] 
-        [ button [ class "info-control", class "directions"
-        --, onClick ShowDirectionsOptions 
-        ] [ text "Directions" ]
-        , button [ class "info-control", class "close", onClick CloseLandmarkSummary ] [ text "Close" ]
+viewRoute : Model -> Html Msg
+viewRoute model =
+    div [ class "info-container" ] 
+        [ viewRouteViewControls
+        , viewTransportControls model.transport
+        , viewRouteResults model.mapRoutes
+        ]
+
+
+viewRouteViewControls : Html Msg
+viewRouteViewControls =
+    div [ class "info-controls-container"]
+        [ button 
+            [ classList 
+                [ ( "info-control", True )
+                , ( "close", True )
+                ]
+            ]
+            [ text "Cancel" ]
+        , button
+            [ classList
+                [ ( "info-control", True )
+                , ( "start-navigation", True )
+                , ( "button-disabled", True )
+                ]
+            , disabled True
+            ]
+            [ text "Go" ]
+        ]
+
+
+viewTransportControls : Transport -> Html Msg
+viewTransportControls transport =
+    let 
+        isCarChosen = 
+            case transport of
+                Car ->
+                    True
+
+                Walk ->
+                    False
+    in
+    div [ class "info-controls-container"]
+        [ button 
+            [ classList 
+                [ ( "info-control", True )
+                , ( "transport", True )
+                , ( "selected-transport", not isCarChosen )
+                ]
+            ]
+            [ text "Car" ]
+        , button
+            [ classList
+                [ ( "info-control", True )
+                , ( "transport", True )
+                , ( "selected-transport", isCarChosen )
+                ]
+            ]
+            [ text "Walk" ]
+        ]
+
+viewRouteResults : MapRoutes -> Html Msg
+viewRouteResults mapRoute =
+    case mapRoute of 
+        RoutesUnavailable ->
+            p [ style "text-align" "center" ] [ text "No Routes available" ]
+
+        RoutesCalculating ->
+            p [ style "text-align" "center" ] [ text "Calculating Routes..." ]
+
+        RoutesResponse ->
+            ul [ id "routes-results" ] []
+                --(List.map (viewRouteSuggestion "" )
+
+viewRouteSuggestion : Html Msg
+viewRouteSuggestion =
+    div [] []
+
+
+
+
+
+-- DIRECTIONS
+
+
+viewDirection : Model -> Html Msg
+viewDirection { startPoint, endPoint, redactedRoutePoint, addressResults } =
+    div [ class "info-container" ]
+        [ viewDirectionsControls (startPoint, endPoint)
+        , viewAddressInputs (startPoint, endPoint)
+        , viewAddressResults addressResults redactedRoutePoint
+        ]
+
+
+viewDirectionsControls : ( RoutePoint, RoutePoint ) -> Html Msg
+viewDirectionsControls ( startPoint, endPoint ) =
+    case ( startPoint, endPoint ) of
+        ( StartPointValid _ _, EndPointValid _ _ ) ->
+            infoControlsContainer False
+
+        ( _, _ ) ->
+            infoControlsContainer True
+
+
+infoControlsContainer : Bool -> Html Msg
+infoControlsContainer routeAccess =
+    div [ class "info-controls-container" ]
+        [ button
+            [ classList
+                [ ( "info-control", True )
+                , ( "close", True )
+                ]
+            , onClick InfoClose
+            ]
+            [ text "Cancel" ]
+        , button
+            [ classList
+                [ ( "info-control", True )
+                , ( "directions", True )
+                , ( "button-disabled", routeAccess )
+                ]
+            , disabled routeAccess
+            , onClick InfoOpenRoute
+            ]
+            [ text "Route" ]
+        ]
+
+
+viewAddressInputs : (RoutePoint, RoutePoint) -> Html Msg
+viewAddressInputs ( startPoint, endPoint ) =
+    div [ id "address-inputs" ]
+        [ inputWrapperFrom startPoint
+        , inputWrapperTo endPoint
+        ]
+
+
+inputWrapperFrom : RoutePoint -> Html Msg
+inputWrapperFrom startPoint =
+    let
+        isValidAdrress = 
+            case startPoint of
+                StartPointValid _ _ ->
+                    True
+
+                _ ->
+                    False
+    in
+    Html.form [ class "directions-input" ]
+        [ label 
+            [ classList
+                [ ( "valid-address", isValidAdrress) 
+                , ( "invalid-address", not isValidAdrress)
+                ]
+            ] 
+            [ text "From" ]
+        , input
+            [ onInput (\text -> InfoUpdateMapRoute (StartPointInvalid text))
+            , onFocus (InfoUpdateRouteFocus StartPoint)
+            , autofocus True
+            , placeholder "Search"
+            , value (pointToString startPoint)
+            , type_ "search"
+            ]
+            []
+        ]
+
+
+inputWrapperTo : RoutePoint -> Html Msg
+inputWrapperTo endPoint =
+    let
+        isValidAdrress = 
+            case endPoint of
+                EndPointValid _ _ ->
+                    True
+
+                _ ->
+                    False
+    in
+    Html.form [ class "directions-input" ]
+        [ label 
+            [ classList
+                [ ( "valid-address", isValidAdrress) 
+                , ( "invalid-address", not isValidAdrress)
+                ]
+            ] 
+            [ text "To" ]
+        , input
+            [ onInput (\text -> InfoUpdateMapRoute (EndPointInvalid text))
+            , onFocus (InfoUpdateRouteFocus EndPoint)
+            , placeholder "Search"
+            , value (pointToString endPoint)
+            , type_ "search"
+            ]
+            []
+        ]
+
+
+viewAddressResults : AddressResults -> RedactedPoint -> Html Msg
+viewAddressResults results redactedPoint =
+    case results of
+        AddressResultsLoaded items ->
+            ul [ id "address-results" ]
+                (List.map (viewAddressSuggestion redactedPoint) items )
+
+        AddressResultsEmpty ->
+            p [ style "text-align" "center" ] [ text "No results" ]
+
+        AddressResultsLoading ->
+            p [ style "text-align" "center" ] [ text "Loading..." ]
+
+        AddressResultsErr err ->
+            p [ style "text-align" "center" ] [ text "Fetching suggestions has failed" ]
+
+
+viewAddressSuggestion : RedactedPoint -> Item -> Html Msg
+viewAddressSuggestion redactedPoint item =
+    let 
+        _ = Debug.log "item" item
+        point =
+            case redactedPoint of
+                StartPoint ->
+                    StartPointValid item.address.label item.position
+
+                EndPoint -> 
+                    EndPointValid item.address.label item.position
+    in
+    li
+        [ class "address-suggestion"
+        , attribute "data-lan" (String.fromFloat item.position.lat)
+        , attribute "data-lng" (String.fromFloat item.position.lng)
+        , attribute "data-title" item.address.label
+        , onClick (InfoUpdateMapRoute point)
+        --, onMouseEnter (MapMarkerAddDefault item.position)
+        ]
+        [ p [ class "address-name" ] [ text item.address.label ]
+        , p [ class "address-details" ] 
+            [ text 
+                ( item.address.county 
+                    ++ " | "
+                    ++ item.address.district
+                    ++ " | "  
+                    ++ item.address.countryName) ]
+        ]
+
+
+
+-- Summary
+
+
+viewSummary : SummaryType -> Html Msg
+viewSummary summaryType =
+    case summaryType of
+        SummaryValid summary ->
+            div [ class "info-container" ]
+                [ viewInfoControls summary
+                , div [ id "summary" ]
+                    [ viewTitle summary.title
+                    , viewImage summary
+                    , viewText summary.extract
+                    , viewWikiLink summary.wikiUrl
+                    ]
+                ]
+
+        SummaryInvalid ->
+            div [ id "summary-container" ] [ text "No info" ]
+
+
+viewInfoControls : Summary -> Html Msg
+viewInfoControls landmark =
+    div [ class "info-controls-container" ]
+        [ button [ class "info-control", class "close", onClick InfoClose ] [ text "Close" ]
+        , button
+            [ class "info-control"
+            , class "directions"
+            , onClick (InfoOpenDirections landmark.title landmark.coordinates)
+            ]
+            [ text "Directions" ]
         ]
 
 
@@ -449,29 +713,49 @@ viewTitle title =
     h3 [ id "summary-title" ] [ text title ]
 
 
-viewImage : LandmarkSummary -> Html Msg
+viewImage : Summary -> Html Msg
 viewImage landmark =
     let
-        image = if (landmark.originalImage == "") then
-                    p [] [ text "No Image" ]
-                else
-                    img 
-                        [ id "summary-image"
-                        , alt landmark.title
-                        , src landmark.thumbnail 
-                        ]
-                        []       
+        image =
+            if landmark.originalImage == "" then
+                p [] [ text "No Image" ]
+
+            else
+                img
+                    [ id "summary-image"
+                    , alt landmark.title
+                    , src landmark.thumbnail
+                    ]
+                    []
     in
-    div [ id "summary-image" ]
-        [ image ]
+    div [ id "summary-image" ] [ image ]
 
 
 viewText : String -> Html msg
 viewText summaryText =
-    div [ id "summary-text" ]
-        [ text summaryText ] 
+    div [ id "summary-text" ] [ text summaryText ]
 
 
 viewWikiLink : String -> Html msg
 viewWikiLink url =
-    a [ id "summary-wiki" ,href url  ] [ text "Open Wiki Page" ]
+    a [ id "summary-wiki", href url ] [ text "Open Wiki Page" ]
+
+
+
+-- HELPERS
+
+
+pointToString : RoutePoint -> String
+pointToString point =
+    case point of
+        StartPointInvalid title ->
+            title
+
+        StartPointValid title position ->
+            title ++ ", " ++ positionToString position
+
+        EndPointInvalid title ->
+            title
+
+        EndPointValid title position ->
+            title ++ ", " ++ positionToString position
