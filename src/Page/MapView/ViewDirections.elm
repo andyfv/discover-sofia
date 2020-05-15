e467cdb86927cb5a2a3a48425d216df2193bf681
Andy Filchev <andyfilchev@protonmail.com>
2020-05-16 01:00:37 +0300

    feat: Move Map viewDirections to its own module

---
+++ src/Page/MapView/ViewDirections.elm
@@ -0,0 +1,20 @@
+port module ViewDirections exposing (Model, Msg, OutMsg(..), view, update, init, subscriptions)
+
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (on, onClick, onInput, onMouseEnter, onMouseLeave, onFocus)
+import Json.Decode exposing (Error, Value, decodeValue)
+
+
+
+
+
+
+
+
+
+
+
+
+
