56a86f6ab619770e52563fd6d375a5dcad706aad
Andy Filchev <andyfilchev@protonmail.com>
2020-06-10 20:30:44 +0300

    feat : Add Photo.elm page to handle photo uploads and predicting the photos

--- src/Page/Photos.elm
+++ src/Page/Photos.elm
@@ -1,31 +1,52 @@
-module Page.Photos exposing (Model, Msg, view, init, update)
+module Page.Photos exposing (Model, Msg, view, init, update, subscriptions)
 
-import Html exposing (Html, div)
-import Html.Attributes exposing (id)
---import Article exposing (Article, ArticleCard, Image)
---import Page exposing (viewCards)
+import Html exposing (Html, div, input, text, button, img, p, figure, figcaption)
+import Html.Attributes exposing (..)
+import Html.Events exposing (on, onClick)
+import File exposing (File)
+import File.Select as Select
+import Json.Decode as D
+import Task
+import TF as TF 
 
---import Projects.NeighborhoodHere as NH exposing (..)
---import Projects.SymbolRecognition as SR exposing (..)
---import Projects.SailfishOS as SOS exposing (..)
 
+
+--SUBSCRIPTIONS
+subscriptions : Sub Msg
+subscriptions =
+    Sub.batch
+        [ Sub.map TFStatusMsg (TF.tfStatus (\statusMsg -> statusMsg) )
+        , Sub.map TFPrediction (TF.tfPredictResult (D.decodeValue TF.predictionDecoder))
+        ]
 
 
 -- MODEL
 
 
 type alias Model =
-    { isMapLoaded : Bool
-    , isLandmarkSelected : Bool
+    { image : Maybe File
+    , imageUrl : Maybe String
+    , tfStatus : TF.TFStatus
+    , prediction : TF.PredictionResult
     }
 
 
-init : (Model, Cmd msg)
-init =
-    ( { isMapLoaded = False
-      , isLandmarkSelected = False
+init : TF.TFStatus -> (Model, Cmd msg)
+init tfStatus =
+    let
+        (cmds, newTFStatus) = case tfStatus of
+                                TF.NotLoaded ->
+                                    (TF.tfLoad (), TF.Loading)
+
+                                _ -> 
+                                    (Cmd.none, tfStatus)
+    in
+    ( { image = Nothing
+      , imageUrl = Nothing
+      , tfStatus = newTFStatus
+      , prediction = TF.Empty
       }
-    , Cmd.none    
+    , Cmd.batch [ cmds ]
     )
 
 
@@ -35,19 +56,129 @@ init =
 
 type Msg
     = NoOp
+    | Pick
+    | GotImage File
+    | GotPreview String
+    | TFStatusMsg Bool
+    | TFPrediction (Result D.Error TF.PredictionResult)
 
-
-update : Msg -> Model -> (Model, Cmd msg)
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
     case msg of
         NoOp ->
-            (model, Cmd.none)
+            ( model, Cmd.none )
 
+        Pick ->
+            ( model
+            , Select.file ["image/*"] GotImage
+            )
+
+        GotImage image ->
+            ( { model | image = (Just image) } 
+            , Task.perform GotPreview <| File.toUrl image
+            )
+
+        GotPreview url ->
+            ( { model | imageUrl = (Just url) }
+            , TF.tfImagePredict (url)
+            )
+
+        TFStatusMsg statusMsg ->
+            case statusMsg of 
+                True ->
+                    ( { model | tfStatus = TF.Loaded }, Cmd.none )
+
+                False ->
+                    ( { model | tfStatus = TF.NotLoaded}, Cmd.none )
+
+
+        TFPrediction (Ok result) ->
+            ( { model | prediction = result }, Cmd.none )
+
+        TFPrediction (Err err) ->
+            ( { model | prediction = TF.PredictionErr "Error parsing the prediction" }, Cmd.none)
 
 
 -- VIEW
 
 
-view : Model -> Html msg
-view model  =
-    div [ id "mapContainer" ] []
+view : Model -> Html Msg
+view model =
+    div [ id "photo-page"]
+        [ div [ id "photo-results-wrapper" ] 
+            [ viewUploadButton model.tfStatus
+            , viewPreview model.imageUrl model.prediction
+            ]
+        ]
+
+
+viewUploadButton : TF.TFStatus -> Html Msg 
+viewUploadButton modelStatus =
+    let 
+        wrapper : List (Html.Attribute Msg) -> List (Html Msg) -> Html Msg
+        wrapper listAttr listHtml = 
+            div [ id "upload-button" ]
+                [ button listAttr listHtml
+                ]
+    in
+    case modelStatus of 
+        TF.Loaded ->
+            wrapper 
+                [ class "btn-upload", onClick Pick ] 
+                [ text "Upload Photo" ]
+
+        TF.Loading ->
+            wrapper 
+                [ classList  
+                    [ ("btn-upload", True)
+                    , ("button-disabled", True)
+                    ]
+                , disabled True 
+                ] 
+                [ text "Loading model" ]
+
+        TF.NotLoaded ->
+            wrapper
+                [ classList  
+                    [ ("btn-upload", True) 
+                    , ("button-disabled", True)
+                    ]
+                , disabled True 
+                ]
+                [ text "Model Not Loaded" ]
+
+
+viewPreview : Maybe String -> TF.PredictionResult -> Html Msg
+viewPreview maybeUrl result =
+    case (maybeUrl, result) of
+        (Just url, TF.Prediction data) ->
+            figure [ id "photo-preview-wrapper" ]
+                [ img 
+                    [ id "photo-to-predict"
+                    , src url
+                    , alt "Selected Photo" 
+                    ] 
+                    []
+                , figcaption [] 
+                    [ p [] [ text (data.className) ]
+                    , p [] 
+                        [ text ("Accuracy: " ++ data.percentage)
+                        ]
+                    ]
+                ]
+
+        (Just url, TF.PredictionErr err) ->
+            figure [ id "photo-preview-wrapper" ]
+                [ img 
+                    [ id "photo-to-predict"
+                    , src url
+                    , alt "Selected Photo" 
+                    ] 
+                    []
+                , figcaption [] 
+                    [ text err ]
+                ]
+
+        (_, _) ->
+            text ""
+
