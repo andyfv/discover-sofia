101db8686319e090c79f082e8e81edd35e74e888
Andy Filchev <andyfilchev@protonmail.com>
2020-07-21 12:26:38 +0300

    refactor: Change model from uint8 to uint16

--- js/TensorFlow.js
+++ js/TensorFlow.js
@@ -81,7 +81,7 @@ function loadLibrary(url) {
  */
 export async function loadModel(onLoad) {
     try {
-        model = await tf.loadGraphModel('/assets/tfjs_model_quantized_uint8/model.json');
+        model = await tf.loadGraphModel('/assets/tfjs_model_quantized_uint16/model.json');
     } catch (e) {
         // Notifies Elm runtime that the loading has failed
         onLoad(false);
