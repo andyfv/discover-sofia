b77416a1539a634c99fb0c2fc21f60291324b468
Andy Filchev <andyfilchev@protonmail.com>
2020-10-09 20:43:43 +0300

    chore: Clean unused code

--- src/ViewPort.elm
+++ src/ViewPort.elm
@@ -2,10 +2,6 @@ module Viewport exposing
     ( view
     , viewNotFound
     , viewContent
-    --, viewCards
-    --, viewCard
-    --, viewCardImage
-    --, viewCardInfo
     , Config
     )
 
@@ -13,7 +9,6 @@ import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Route exposing (Route(..), internalLink)
---import Article exposing (Article, ArticleCard, Image)
 
 
 type alias Config msg =
@@ -49,61 +44,6 @@ viewContent content route =
             [ content ]
 
 
-
---viewCards :  List ArticleCard -> Html msg
---viewCards articles =
---    div [ id "view-cards" ] ( List.map viewCard articles )
-
-
---viewCard : ArticleCard -> Html msg
---viewCard article =
---    a 
---        [ class "view-card"
---        , href article.href
---        ]
---        [ viewCardImage article.image
---        , viewCardInfo article
---        ]
-
-
---viewCardImage : Image -> Html msg
---viewCardImage image =
---    div [ class "card-image-wrapper" ]
---        [ img 
---            [ class "card-image" 
---            , src image.src
---            , alt image.description
---            ]
---            []
---        ]
-
-
---viewCardInfo : ArticleCard -> Html msg
---viewCardInfo article =
---    div [ class "card-info" ]
---        [ h3 [ class "card-info-header" ] [ text article.title ] 
---        , div [ class "card-info-date" ] [ text article.date ]
---        ]
-    
-
---{- Article Template -}
-
---viewArticle : Article -> Html msg
---viewArticle article =
---    div [ class "article" ]
---        [ h1 [] [ text (Article.getTitle article) ]
---        , h3 [] [ text (Article.getSubtitle article) ]
---        , h5 [] [ text (Article.getDate article) ]
---        ]
-
-
---{- FOOTER -}
-
---viewFooter : Html msg
---viewFooter = 
---    div [ id "footer" ]
---        [ a [ href "https://github.com/andyfv/z-context" ] [ text "This site source code" ]
---        ]
 
 
 {- FOOTER -}
